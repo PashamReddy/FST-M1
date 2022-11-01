7aff3b77ba858c57962fddc483ffb9ef0c17a7dc
PashamReddy <116861334+PashamReddy@users.noreply.github.com>
2022-11-01 20:20:49 +0530

    Add files via upload

--- Java/Activities/Activity2.java
+++ Java/Activities/Activity2.java
@@ -1,27 +1,27 @@
-package activities;
+import org.example.BankAccount;
+import org.example.NotEnoughFundsException;
+import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class Activity2 {
-    public static void main(String[] args){
-        int[] arr={10,77,10,54,-11,10};
-        System.out.println("original arr numbers:"+ Arrays.toString(arr));
-        int find_arr=10;
-        int final_value=30;
-        System.out.println("final result:"+ result(arr,find_arr,final_value));
-    }
-    public static boolean result(int[] arr1, int find_arr, int final_value){
-        int arr2=0;
-        for(int i:arr1)        {
-            if(i==find_arr)
-            {
-             arr2=arr2+find_arr;
-            }
-            if(arr2>final_value){
-                break;
-            }
-        }
-        return arr2==final_value;
+class ExpectedExceptionTest {
+
+    @Test
+    void notEnoughFunds() {
+        // Create an object for BankAccount class
+        BankAccount account = new BankAccount(9);
+
+        // Assertion for exception
+        assertThrows(NotEnoughFundsException.class, () -> account.withdraw(10),
+                "Balance must be greater than amount of withdrawal");
     }
 
+    @Test
+    void enoughFunds() {
+        // Create an object for BankAccount class
+        BankAccount account = new BankAccount(100);
+
+        // Assertion for no exceptions
+        assertDoesNotThrow(() -> account.withdraw(100));
+    }
 }
